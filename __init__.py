e441d35c962a4f55d4ba63822e9e9b464372fc36
Peter Petrik <zilolv@gmail.com>
2016-05-11 12:09:31 +0200

    fix #3

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2015 Martin Dobias
 #-----------------------------------------------------------
 # Licensed under the terms of GNU GPL 2
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -30,6 +30,7 @@ def classFactory(iface):
     return FirstAidPlugin(iface)
 
 
+
 class FirstAidPlugin:
     def __init__(self, iface):
         self.old_show_exception = None
@@ -39,6 +40,9 @@ class FirstAidPlugin:
         # hook to exception handling
         self.old_show_exception = qgis.utils.showException
         qgis.utils.showException = showException
+
+        # ReportPlugin also hooks exceptions and needs to be loaded after FirstAid
+        qgis.utils.reloadPlugin("qgis-report-plugin")
 
     def unload(self):
 
