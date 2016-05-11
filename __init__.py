0a733d9115a87475e8d0b3b5920bb2ca2f66286f
Peter Petrik <zilolv@gmail.com>
2016-05-11 14:13:45 +0200

    fix #3 (#4)
    Allow cooperation with Report plugin (fixes #3)

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2015 Martin Dobias
 #-----------------------------------------------------------
 # Licensed under the terms of GNU GPL 2
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -35,10 +35,21 @@ class FirstAidPlugin:
         self.old_show_exception = None
 
     def initGui(self):
+        # ReportPlugin also hooks exceptions and needs to be unloaded if active
+        # so qgis.utils.showException is the QGIS native one
+        report_plugin = "report"
+        report_plugin_active = report_plugin in qgis.utils.active_plugins
+        if report_plugin_active:
+            qgis.utils.unloadPlugin(report_plugin)
 
         # hook to exception handling
         self.old_show_exception = qgis.utils.showException
         qgis.utils.showException = showException
+
+        # If ReportPlugin was activated, load and start it again to cooperate
+        if report_plugin_active:
+            qgis.utils.loadPlugin(report_plugin)
+            qgis.utils.startPlugin(report_plugin)
 
     def unload(self):
 
