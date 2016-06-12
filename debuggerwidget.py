59b12a319aafc2b287ce088c75d0aab0ef32098d
Martin Dobias <wonder.sk@gmail.com>
2016-06-12 19:34:15 +0200

    Show line numbers in debugger's source code viewer

--- debuggerwidget.py
+++ debuggerwidget.py
@@ -128,6 +128,19 @@ class Debugger(object):
             print "trace", format_frames(frame), " | ", event, arg
 
 
+class LineNumberArea(QWidget):
+
+    def __init__(self, editor):
+        QWidget.__init__(self, editor)
+        self.editor = editor
+
+    def sizeHint(self):
+        return QSize(self.editor.lineNumberAreaWidth(), 0)
+
+    def paintEvent(self, event):
+        self.editor.lineNumberAreaPaintEvent(event)
+
+
 class SourceWidget(QPlainTextEdit):
     def __init__(self, filename, parent=None):
         QTextEdit.__init__(self, parent)
@@ -146,9 +159,63 @@ class SourceWidget(QPlainTextEdit):
 
         self.highlighter = PythonHighlighter(self.document())
 
+        # line numbers support
+        self.lineNumberArea = LineNumberArea(self)
+        self.blockCountChanged.connect(self.updateLineNumberAreaWidth)
+        self.updateRequest.connect(self.updateLineNumberArea)
+        self.updateLineNumberAreaWidth(0)
+
         self.filename = filename
         self.breakpoints = []
         self.debug_line = -1
+
+    ### support for line numbers - start
+
+    def lineNumberAreaWidth(self):
+        digits = 1
+        max_digit = max(1, self.blockCount())
+        while max_digit >= 10:
+            max_digit /= 10
+            digits += 1
+        return self.fontMetrics().width('9') * (digits+2)
+
+    def updateLineNumberAreaWidth(self, newBlockCount):
+        self.setViewportMargins(self.lineNumberAreaWidth(), 0, 0, 0)
+
+    def updateLineNumberArea(self, rect, dy):
+        if dy:
+            self.lineNumberArea.scroll(0, dy)
+        else:
+            self.lineNumberArea.update(0, rect.y(), self.lineNumberArea.width(), rect.height())
+
+        if rect.contains(self.viewport().rect()):
+            self.updateLineNumberAreaWidth(0)
+
+    def resizeEvent(self, e):
+        QPlainTextEdit.resizeEvent(self, e)
+        cr = self.contentsRect()
+        self.lineNumberArea.setGeometry(QRect(cr.left(), cr.top(), self.lineNumberAreaWidth(), cr.height()))
+
+    def lineNumberAreaPaintEvent(self, event):
+        painter = QPainter(self.lineNumberArea)
+        painter.fillRect(event.rect(), Qt.white)
+        painter.fillRect(QRect(event.rect().right()-1, event.rect().top(), 1, event.rect().height()), Qt.lightGray)
+        block = self.firstVisibleBlock()
+        blockNumber = block.blockNumber()
+        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top()
+        bottom = top + self.blockBoundingRect(block).height()
+
+        while block.isValid() and top <= event.rect().bottom():
+            if block.isVisible() and bottom >= event.rect().top():
+                painter.setPen(Qt.black)
+                painter.drawText(0, top, self.lineNumberArea.width() - self.fontMetrics().width('9'),
+                                 self.fontMetrics().height(), Qt.AlignRight, str(blockNumber+1))
+            block = block.next()
+            top = bottom
+            bottom = top + self.blockBoundingRect(block).height()
+            blockNumber += 1
+
+    ### support for line numbers - finish
 
     def toggle_breakpoint(self):
         line_no = self.textCursor().blockNumber()
