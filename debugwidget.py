a2eed6f205f1685846935585f19cb930926fa756
Zayne Tomlins <zaynetomlins@gmail.com>
2023-02-09 12:58:52 +1000

    Long error messages now wrap around

--- debugwidget.py
+++ debugwidget.py
@@ -323,6 +323,7 @@ class DebugWidget(QWidget):
 
         msg = str(value).replace("\n", "<br>").replace(" ", "&nbsp;")
         self.error = QLabel("<h1>"+etype.__name__+"</h1><b>"+msg+"</b>")
+        self.error.wordWrap(True)
         self.error.setTextInteractionFlags(Qt.TextSelectableByMouse)
 
         self.frames = FramesView()
